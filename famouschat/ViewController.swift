3746ebcec448a1d492c9c0cba36654f59f5c06cb
angel oni <Oni@zjgs-iMac.local>
2018-11-04 20:38:08 +0800

    Initial Commit

---
+++ famouschat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  famouschat
+//
+//  Created by Oni Angel on 04/11/2018.
+//  Copyright © 2018 Oni Angel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
